f644ca97c4717dc41c70f2c96d4cc01182c6b56d
Mattin Guiheneuf <mguiheneuf@iutbayonne.univ-pau.fr>
2025-01-16 15:40:54 +0100

    Modifications de sécurité

--- S5DevBack/DevLaravel/routes/web.php
+++ S5DevBack/DevLaravel/routes/web.php
@@ -108,7 +108,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
+
+Route::get('login', [\App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [\App\Http\Controllers\Auth\LoginController::class, 'login'])->middleware('throttle:5,1');
+Route::post('logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
+Route::get('register', [\App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [\App\Http\Controllers\Auth\RegisterController::class, 'register'])->middleware('throttle:3,1');
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
